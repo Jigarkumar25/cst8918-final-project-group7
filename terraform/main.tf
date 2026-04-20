11c30818cdfbc16b58a72e7b8ad404b6deda0d68
Jigarkumar25 <pate1595@algonquinlive.com>
2026-04-19 22:03:38 -0400

    Add AKS module and deploy test cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
 module "network" {
   source = "./modules/network"
 }
+
+module "aks" {
+  source              = "./modules/aks"
+  resource_group_name = module.network.resource_group_name
+  test_subnet_id      = module.network.test_subnet_id
+  prod_subnet_id      = module.network.prod_subnet_id
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,3 +21,10 @@ output "dev_subnet_id" {
 output "admin_subnet_id" {
   value = module.network.admin_subnet_id
 }
+output "test_cluster_name" {
+  value = module.aks.test_cluster_name
+}
+
+output "prod_cluster_name" {
+  value = module.aks.prod_cluster_name
+}
